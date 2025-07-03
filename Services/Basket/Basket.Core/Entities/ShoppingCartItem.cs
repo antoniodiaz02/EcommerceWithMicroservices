0f3bcbf0313cb3dd6365ed7b130b7900c390df99
Antonio Diaz <mesidiez@gmail.com>
2025-07-03 10:36:25 +0200

    New microservice for cart

---
+++ Services/Basket/Basket.Core/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Core.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quanty { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ProductId { get; set; }
+
+        public string ImageFile { get; set; }
+
+        public string ProductName { get; set; }
+
+    }
+}
---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string UserName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+
+
+    }
+}
